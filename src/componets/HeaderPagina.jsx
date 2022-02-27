2a7c2921bf90b811e35ee89817bde3415c9ffda8
MaxPrain <alder.velasquezobando@gmail.com>
2022-02-27 22:29:29 +0100

    Errores de navegacion

--- src/componets/HeaderPagina.jsx
+++ src/componets/HeaderPagina.jsx
@@ -9,7 +9,7 @@ class HeaderPagina extends React.Component {
         return (
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand href="/grupo5-react">
                         😶‍🌫️ Grupo 5
                     </Navbar.Brand>
                     <Nav className="me-auto">
@@ -23,7 +23,7 @@ class HeaderPagina extends React.Component {
                     </Nav>
                     <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
-                            Logueado como : <a href="/perfil">{localStorage.getItem('user')}</a>
+                            Logueado como : <a href="/grupo5-react/perfil">{localStorage.getItem('user')}</a>
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
